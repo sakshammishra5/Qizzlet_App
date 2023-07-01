3640b6714e88d8721d6a6618cedfd86262215626
sakshammishra5 <sakshammishra5thsep@gmail.com>
2023-07-01 09:54:49 +0530

    initial commit in version 4

--- src/Components/Answer.jsx
+++ src/Components/Answer.jsx
@@ -2,30 +2,11 @@ import { useState } from 'react'
 import './styles/Particularans.css'
 
 const Answer = (props) => {
-  const { optn, correct_answer, incorrect_answers } = props
-  const [selected, setselected] = useState('not_selected')
-
-  function Handleonclick(ev) {
-    if (ev.target.className == "optn") {
-      console.log( ev.target.attributes.selectedornot.value)
-      ev.target.attributes.selectedornot.value=setselected("selected_ans")
-    }
-    addclass(ev)
-  }
-  
-  function addclass(ev) {
-    if (selected == "selected_ans") {
-      ev.target.classList.add("selected_ans")
-    }
-    else if(selected=="not_selected"){
-      ev.target.classList.remove("selected_ans")
-    }
-
-  }
+  const {id,value,picked, correct_answer, incorrect_answers,handleonclick } = props
 
   return (
-    <div className='optn' selectedornot={selected} onClick={Handleonclick} >
-      {optn}
+    <div className='optn' id={id} onClick={handleonclick} picked={picked.toString()}>
+      {value}
     </div>
   )
 }
--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -1,27 +1,34 @@
 import { useContext, useState } from "react"
+import { nanoid } from 'nanoid'
 import Answer from "./answer"
 
 
 const Options = (props) => {
-  const { incorrect_answers, correct_answer } = props
- 
+  const { incorrect_answers, correct_answer} = props
+  const [selected, setSelected] = useState(false)
+
+  function handleonclick(ev){
+   }
+    
+
+  
+  let optnArr=incorrect_answers.map((item, indx) =>
+   <Answer
+    key={indx}
+    value={item}
+    id={nanoid()} 
+    correct_answer={correct_answer} 
+    incorrect_answers={incorrect_answers} 
+    handleonclick={handleonclick}
+    picked={selected}
+     />)
   
   return (
-    <div>{incorrect_answers.map((item, indx) => <Answer key={indx} optn={item} correct_answer={correct_answer} incorrect_answers={incorrect_answers} />)}</div>
+    <div>
+      {optnArr}
+    </div>
     )
 }
 
-// if(ev.target.innerText==correct_answer){
-//   console.log("sahi ans")
-// }
-// if(ev.target.className=="optn"){
-//  console.log( ev.target.attributes.alagattr)
-//   if(ev.target.attributes.alagattr.value==optnStatus){
-//     setoptnStatus((prev)=>prev="selected_ans")
-//     if(ev.target.attributes.alagattr.value=="selected_ans"){
-//       ev.target.classList.add("selected_ans")
-//     }
-//   }
-// }
 
 export default Options
